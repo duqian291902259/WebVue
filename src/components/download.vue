0a7b2984b6d1b0a9e897bd206af0285a42df5578
Dusan <duqian2010@gmail.com>
2021-09-14 21:33:11 +0800

    add el-link to download ec file

--- src/components/download.vue
+++ src/components/download.vue
@@ -2,7 +2,18 @@
   <div>
     <h1 style="text-align: center">下载覆盖率报告文件</h1>
 
-    <el-button style="text-align: center" @click="download">下载覆盖率报告文件</el-button>
+    <el-button style="text-align: center" @click="download"
+      >下载覆盖率报告文件</el-button
+    >
+
+    <el-link
+      type="success"
+      herf="http://127.0.0.1:8090/download?path=download/cc-android/3.8.1/coverage.ec"
+      @click="download2"
+      >coverage.ec</el-link
+    >
+
+    <!-- <a style="text-align: center;margin-top:20px;float:left;" herf="http://127.0.0.1:8090/download?path=download/cc-android/3.8.1/coverage.ec">coverage.ec</a> -->
   </div>
 </template>
 <script>
@@ -11,10 +22,15 @@ export default {
     return {};
   },
   methods: {
-    download(){
-      window.open("http://127.0.0.1:8090/download/android/3.8.1/duqian.png")
+    download() {
+      window.open("http://127.0.0.1:8090/download/android/3.8.1/duqian.png");
       //window.open("http://127.0.0.1:8090/WebServer/JacocoApi/queryEcFile?appName=android&versionCode=3.8.1")
-    }
+    },
+    download2() {
+      window.open(
+        "http://127.0.0.1:8090/download?path=download/cc-android/3.8.1/coverage.ec"
+      );
+    },
   },
 };
 </script>
